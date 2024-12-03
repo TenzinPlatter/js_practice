3040b11d7ecb912bd40d60bcb69e4791b9199037
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2024-12-04 10:35:41 +1100

    Finished dropdown

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,67 @@
-function greet(name: string): string {
-  return `Hello, ${name}!`;
+import "./styles.css"
+
+const body = document.querySelector("body")!;
+const dropdown = createDropDown();
+const content = dropdown.querySelector(".content");
+
+for (let i = 0; i < 3; i++) {
+	const div = document.createElement("button");
+	div.textContent = i.toString();
+	content?.appendChild(div);
 }
 
-const user = 'TypeScript Developer';
+body.appendChild(dropdown);
 
-console.log(greet(user));
+
+function createDropDown(): HTMLElement {
+	const container = <HTMLElement>document.createElement("div");
+	const button = <HTMLElement>document.createElement("div");
+	const content = <HTMLElement>document.createElement("div");
+	let isShown = false;
+
+	container.appendChild(button);
+	container.appendChild(content);
+
+	container.classList.add("dropdown-container");
+	button.classList.add("button");
+	content.classList.add("content");
+
+	for (let i = 0; i < 3; i++) {
+		const bar = document.createElement("div");
+		bar.classList.add("bar");
+		bar.style.border = "1px solid grey";
+		button.appendChild(bar);
+	}
+
+	// styling
+	button.style.width = "30px";
+	button.style.height = "30px";
+	button.style.display = "flex";
+	button.style.flexDirection = "column";
+	button.style.justifyContent = "space-evenly";
+
+	container.style.position = "relative";
+
+	content.style.position = "absolute";
+	content.style.flexDirection = "column";
+	content.style.gap = "8px";
+	content.style.top = "36px";
+	content.style.width = "120px";
+	// using method so that calc() works
+	content.style.left = "0";
+	content.style.background = "#d3d3d3";
+	content.style.padding = "16px";
+	content.style.display = "none";
+
+	button.addEventListener("click", () => {
+		if (isShown) {
+			content.style.display = "none";
+		} else {
+			content.style.display = "flex";
+		}
+
+		isShown = !isShown;
+	});
+
+	return container;
+}
